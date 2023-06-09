6d49cdd30b5642c985dc6cd586f5b0246bbe0185
Ilya Mikhasik <imikhassik@gmail.com>
2023-06-09 17:08:11 +0300

    Clean up

--- FSTR/pereval/views.py
+++ FSTR/pereval/views.py
@@ -1,5 +1,5 @@
 from rest_framework.response import Response
-from rest_framework import mixins, status
+from rest_framework import mixins
 from rest_framework import generics
 from rest_framework.exceptions import ValidationError
 
